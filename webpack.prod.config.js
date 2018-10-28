e6b620425135925a0f71621a1cd37caab2e29261
Oskar Szura <oskar.szura@gmail.com>
2018-10-28 08:42:49 +0100

    GRAPHN-8 - Implement flexbox and relations to body... (#6)
    * Update c-body to flex columns

---
+++ webpack.prod.config.js
@@ -0,0 +1,54 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  context: `${__dirname}/src`,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: '[name].js',
+  },
+  entry: {
+    scripts: './index.js',
+    css: './style.scss',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      resolve: {
+        extensions: ['.js', '.jsx'],
+      },
+      use: {
+        loader: 'babel-loader',
+      },
+    }, {
+      test: /\.scss$/,
+      use: [
+        devMode ? 'style-loader' : {
+          loader: MiniCssExtractPlugin.loader,
+        },
+        'css-loader',
+        'sass-loader',
+      ],
+    }, {
+      test: /\.jpg|.png/,
+      loader: 'file-loader',
+    }],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }, { allChunks: true }),
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
+};
