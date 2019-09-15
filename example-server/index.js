30e2256e9b4ca6df995a04b0f6f52d5c51f8ba93
Oskar Szura <oskar.szura@gmail.com>
2019-09-15 18:59:07 +0200

    CODA-19 - Add Cypress integration tests

--- example-server/index.js
+++ example-server/index.js
@@ -1,2 +1,18 @@
-const ExampleServer = require('./ExampleServer');
-new ExampleServer();
+const fs = require('fs');
+const Koa = require('koa');
+const Router = require('koa-router');
+const serve = require('koa-static');
+
+const app = new Koa();
+const router = new Router();
+const template = fs.readFileSync('./public/module.html');
+
+router.get("/", ctx => {
+  ctx.type = 'html';
+  ctx.body = template;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+app.use(serve(`${__dirname}/../public`));
+
+app.listen(3000, () => {});
