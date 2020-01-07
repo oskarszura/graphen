53ac49a6d95cc36c67b74b717ada281d753ac72e
Oskar Szura <oskar.szura@gmail.com>
2020-01-07 16:38:06 +0100

    CODA-48 - Move icons and 'Icon' component

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,17 +1,17 @@
 // @flow
-import _ from 'lodash';
-import React from 'react';
-import classNames from 'classnames';
+import _ from "lodash";
+import React from "react";
+import classNames from "classnames";
 
 type Props = {
   type: string,
-  className: string,
+  className: string
 };
 
 const Icon = (props: Props) => {
   const { type, className } = props;
 
-  const classes = classNames('c-icon', className, {
+  const classes = classNames("c-icon", className, {
     [`c-icon--${type}`]: !_.isEmpty(type)
   });
 
