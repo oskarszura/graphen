4c635d27fe069554b2d7563ae92bcda3649b404c
Bartosz Szura <bartosz.szura100@gmail.com>
2020-07-20 08:45:40 +0200

    refactoring CSS

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -14,10 +14,10 @@ export default function Dropdown(props) {
         {dropdown}
   		</button>
       {isExpanded && (
-        <div className="dropdown__content">
-          <ul>
+        <div className="dropdown-content">
+          <ul className="dropdown-content__list">
               {items.map(item => (
-        <li><a href="#">{item.value}</a></li>
+        <li className="dropdown-content__item"><a className="dropdown-content__link" href="#">{item.value}</a></li>
         ))}
            </ul>
         </div>
