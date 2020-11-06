7dda14474c485b2b833247fcf81442f530970436
Bartosz Szura <bartosz.szura100@gmail.com>
2020-11-06 09:03:16 +0100

    update

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -37,22 +37,27 @@ export default function Dropdown(props: Props) {
             <ul className="gc-dropdown__list">
               {/* eslint-disable jsx-a11y/no-static-element-interactions */}
               {_.map(items, item => {
-                if (item == items[0]) {return (<li
-                  className="gc-dropdown__item--first"
-                  key={`dropdown-${label}-${item.label}`}
-                  onClick={() => selectItem(item)}
-                >
-                  <a className="gc-dropdown__link">{item.label}</a>
-                </li>)}
-              else { return(
-                <li
-                  className="gc-dropdown__item"
-                  key={`dropdown-${label}-${item.label}`}
-                  onClick={() => selectItem(item)}
-                >
-                  <a className="gc-dropdown__link">{item.label}</a>
-                </li>
-              )}
+                if (item == items[0]) {
+                  return (
+                    <li
+                      className="gc-dropdown__item--first"
+                      key={`dropdown-${label}-${item.label}`}
+                      onClick={() => selectItem(item)}
+                    >
+                      <a className="gc-dropdown__link">{item.label}</a>
+                    </li>
+                  );
+                } else {
+                  return (
+                    <li
+                      className="gc-dropdown__item"
+                      key={`dropdown-${label}-${item.label}`}
+                      onClick={() => selectItem(item)}
+                    >
+                      <a className="gc-dropdown__link">{item.label}</a>
+                    </li>
+                  );
+                }
               })}
               {/* eslint-enable jsx-a11y/no-static-element-interactions */}
             </ul>
