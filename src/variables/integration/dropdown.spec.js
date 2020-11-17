8478a32102e05d8cddd5f1a54b5762558649775c
Bartosz Szura <bartosz.szura100@gmail.com>
2020-11-17 12:19:47 +0100

    update

--- src/variables/integration/dropdown.spec.js
+++ src/variables/integration/dropdown.spec.js
@@ -1,16 +1,20 @@
-describe('test dropdown menu', () => {
-	it('should open dropdown menu', () => {
-		cy.visit('localhost:3000');
-		cy.get(".gc-dropdown");
-		cy.get(".gc-dropdown__btn").click();
-	});
-	it('should choose value red', () => {
-		cy.get(".gc-dropdown__item").contains("Red").click()
-        cy.get(".gc-dropdown__btn").contains("Red")
-	});
-	it('should reopen and choose value blue', () => {
-		cy.get(".gc-dropdown__btn").click();
-		cy.get(".gc-dropdown__item").contains("Blue").click()
-        cy.get(".gc-dropdown__btn").contains("Blue")
-	});
-})
+describe("test dropdown menu", () => {
+  it("should open dropdown menu", () => {
+    cy.visit("localhost:3000");
+    cy.get(".gc-dropdown");
+    cy.get(".gc-dropdown__btn").click();
+  });
+  it("should choose value red", () => {
+    cy.get(".gc-dropdown__item")
+      .contains("Red")
+      .click();
+    cy.get(".gc-dropdown__btn").contains("Red");
+  });
+  it("should reopen and choose value blue", () => {
+    cy.get(".gc-dropdown__btn").click();
+    cy.get(".gc-dropdown__item")
+      .contains("Blue")
+      .click();
+    cy.get(".gc-dropdown__btn").contains("Blue");
+  });
+});
