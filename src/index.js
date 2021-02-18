5c5bd1067d2be00ae78890a6ac7394cc192d31a0
Oskar Szura <oskar.szura@gmail.com>
2021-02-18 11:30:30 +0100

    CODA-141 - Fix hook call error

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 // @flow
-import Accordion from "./components/Accordion";
-import Button from "./components/Button";
-import Dialog from "./components/Dialog";
-import Icon from "./components/Icon";
-import Input from "./components/Input";
-import Image from "./components/Image";
-import Joystick from "./components/Joystick";
-import Link from "./components/Link";
-import Loader from "./components/Loader";
-import Scroller from "./components/Scroller";
-import Tooltip from "./components/Tooltip";
-import Validation from "./components/Validation";
-import Logo from "./components/Logo";
-import Dropdown from "./components/Dropdown";
-import * as constants from "./variables/constants";
+import Accordion from "src/components/Accordion";
+import Button from "src/components/Button";
+import Dialog from "src/components/Dialog";
+import Icon from "src/components/Icon";
+import Input from "src/components/Input";
+import Image from "src/components/Image";
+import Joystick from "src/components/Joystick";
+import Link from "src/components/Link";
+import Loader from "src/components/Loader";
+import Scroller from "src/components/Scroller";
+import Tooltip from "src/components/Tooltip";
+import Validation from "src/components/Validation";
+import Logo from "src/components/Logo";
+import Dropdown from "src/components/Dropdown";
+import * as constants from "src/variables/constants";
 
 export {
   Accordion,
